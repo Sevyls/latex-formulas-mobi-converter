417e78a64c053a3871f7ada199c700d372e6b261
Michael Auß <michael.auss@gmail.com>
2014-06-04 21:30:24 +0200

    Log4j config

--- latex2mobi-converter/src/main/java/at/ac/tuwien/ims/latex2mobiformulaconv/app/Main.java
+++ latex2mobi-converter/src/main/java/at/ac/tuwien/ims/latex2mobiformulaconv/app/Main.java
@@ -1,14 +1,20 @@
 package at.ac.tuwien.ims.latex2mobiformulaconv.app;
 
+import org.apache.log4j.Logger;
+
 /**
  * @author mauss
  *         Created: 29.04.14 22:55
  */
 public class Main {
+    private static Logger logger = Logger.getRootLogger();
+
     public static void main(String[] args) {
+        logger.debug("main() started.");
         // TODO Apache Commons CLI - configuration
         // TODO Apache Commons CLI - parsing
 
         // TODO Log4J -- Logging to File & stdout
+        logger.debug("main() exit.");
     }
 }
