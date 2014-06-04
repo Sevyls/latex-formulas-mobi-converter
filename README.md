88e445adb8cabf1fbaeb2ee18898ed43eafa076a
Michael Auß <michael.auss@gmail.com>
2014-06-04 23:13:28 +0200

    Application Configuration examples + loading

--- README.md
+++ README.md
@@ -5,8 +5,42 @@ Bachelor Thesis
 Michael Auß
 e0525937@student.tuwien.ac.at
 
-https://www.ims.tuwien.ac.at/topics/239
+[https://www.ims.tuwien.ac.at/topics/239](https://www.ims.tuwien.ac.at/topics/239)
 
 Interactive Media Systems is a multimedia research group at the
 Institute of Software Technology and Interactive Systems, part of the
 Faculty of Informatics at the Vienna University of Technology.
+
+## Libraries used
+
+* Apache Commons CLI - [http://commons.apache.org/proper/commons-cli/](http://commons.apache.org/proper/commons-cli/)
+* Apache Commons Configuration - [http://commons.apache.org/proper/commons-configuration/](http://commons.apache.org/proper/commons-configuration/)
+* Apache Log4j - [http://logging.apache.org/log4j/](http://logging.apache.org/log4j/)
+* JDOM - [http://www.jdom.org/](http://www.jdom.org/)
+
+## Requirements
+
+### Pandoc
+This software makes great use of pandoc's LaTeX to HTML converter. 
+
+Download & install from [http://johnmacfarlane.net/pandoc/](http://johnmacfarlane.net/pandoc/)
+
+**Add the installed pandoc executable/directory to your operating system's PATH variable.**
+
+##### OR: config file
+
+Inside the config file "*configuration.properties*" you can define a permanent  
+> pandoc.exec = "file:///opt/pandoc-1.12.4.2/pandoc"
+
+##### OR: CLI arguments
+
+Ad-hoc configuration is possible by passing the following *optional* command line argument:
+
+> -p / --pandoc-exec <pandoc-filepath>
+
+Notice: this overrides all other previous discussed settings.
+
+
+
+#// TODO rest of documentation.
+
--- latex2mobi-converter/src/main/java/at/ac/tuwien/ims/latex2mobiformulaconv/app/Main.java
+++ latex2mobi-converter/src/main/java/at/ac/tuwien/ims/latex2mobiformulaconv/app/Main.java
@@ -1,5 +1,6 @@
 package at.ac.tuwien.ims.latex2mobiformulaconv.app;
 
+import org.apache.commons.cli.*;
 import org.apache.commons.configuration.Configuration;
 import org.apache.commons.configuration.ConfigurationException;
 import org.apache.commons.configuration.PropertiesConfiguration;
@@ -13,6 +14,7 @@ public class Main {
     private static final String CONFIGURATION_FILENAME = "configuration.properties";
     private static Logger logger = Logger.getRootLogger();
     private static Configuration config;
+    private static Options options;
 
     public static void main(String[] args) {
         logger.debug("main() started.");
@@ -25,8 +27,38 @@ public class Main {
             // TODO Exception handling
         }
 
-        // TODO Apache Commons CLI - configuration
-        // TODO Apache Commons CLI - parsing
+
+        options = new Options();
+
+        options.addOption("i", "input", true, "input file");
+        options.addOption("o", "output", true, "output file");
+        options.addOption("p", "pandoc-exec", true, "pandoc executable location");
+        options.addOption("h", "help", false, "show this help");
+
+
+        CommandLineParser parser = new PosixParser();
+        try {
+            CommandLine cmd = parser.parse(options, args);
+
+            if (cmd.hasOption('h')) {
+                // Show usage, ignore other parameters and quit
+                HelpFormatter formatter = new HelpFormatter();
+                formatter.printHelp("latex2mobi", options);
+                logger.debug("Help called, main() exit.");
+                System.exit(0);
+            }
+
+            // TODO Input File handling
+
+            // TODO Output File/directory handling
+
+            // TODO Pandoc executable handling
+
+
+        } catch (ParseException e) {
+            logger.error(e.getMessage(), e);
+            // TODO Exception handling
+        }
 
 
         logger.debug("main() exit.");
