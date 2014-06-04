af4732315fab94cdb95c038be9d283c1845e6a07
Michael Auß <michael.auss@gmail.com>
2014-06-04 21:53:59 +0200

    Application Configuration examples + loading

--- latex2mobi-converter/src/main/java/at/ac/tuwien/ims/latex2mobiformulaconv/app/Main.java
+++ latex2mobi-converter/src/main/java/at/ac/tuwien/ims/latex2mobiformulaconv/app/Main.java
@@ -1,5 +1,8 @@
 package at.ac.tuwien.ims.latex2mobiformulaconv.app;
 
+import org.apache.commons.configuration.Configuration;
+import org.apache.commons.configuration.ConfigurationException;
+import org.apache.commons.configuration.PropertiesConfiguration;
 import org.apache.log4j.Logger;
 
 /**
@@ -7,14 +10,25 @@ import org.apache.log4j.Logger;
  *         Created: 29.04.14 22:55
  */
 public class Main {
+    private static final String CONFIGURATION_FILENAME = "configuration.properties";
     private static Logger logger = Logger.getRootLogger();
+    private static Configuration config;
 
     public static void main(String[] args) {
         logger.debug("main() started.");
+
+        try {
+            logger.debug("Loading configuration.");
+            config = new PropertiesConfiguration(CONFIGURATION_FILENAME);
+        } catch (ConfigurationException e) {
+            logger.error(e.getMessage(), e);
+            // TODO Exception handling
+        }
+
         // TODO Apache Commons CLI - configuration
         // TODO Apache Commons CLI - parsing
 
-        // TODO Log4J -- Logging to File & stdout
+
         logger.debug("main() exit.");
     }
 }
