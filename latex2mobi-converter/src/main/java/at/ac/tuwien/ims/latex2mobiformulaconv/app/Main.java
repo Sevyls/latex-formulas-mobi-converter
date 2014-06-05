62ed30ad85fc888e21dec6c647ec0e4a85d721b9
Michael Auß <michael.auss@gmail.com>
2014-06-05 21:42:40 +0200

    Resolve Working directory, touch empty config file at first start

--- latex2mobi-converter/src/main/java/at/ac/tuwien/ims/latex2mobiformulaconv/app/Main.java
+++ latex2mobi-converter/src/main/java/at/ac/tuwien/ims/latex2mobiformulaconv/app/Main.java
@@ -3,11 +3,12 @@ package at.ac.tuwien.ims.latex2mobiformulaconv.app;
 import at.ac.tuwien.ims.latex2mobiformulaconv.converter.LatexToHtmlConverter;
 import at.ac.tuwien.ims.latex2mobiformulaconv.converter.PandocLatexToHtmlConverter;
 import org.apache.commons.cli.*;
-import org.apache.commons.configuration.Configuration;
 import org.apache.commons.configuration.ConfigurationException;
 import org.apache.commons.configuration.PropertiesConfiguration;
 import org.apache.log4j.Logger;
 
+import java.net.URISyntaxException;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 
 /**
@@ -17,22 +18,24 @@ import java.nio.file.Paths;
 public class Main {
     private static final String CONFIGURATION_FILENAME = "configuration.properties";
     private static Logger logger = Logger.getRootLogger();
-    private static Configuration config;
+    private static PropertiesConfiguration config;
     private static Options options;
 
     public static void main(String[] args) {
         logger.debug("main() started.");
-
-        //logger.debug(Main.class.getProtectionDomain().getCodeSource().getLocation().toString());
-
+        Path workingDirectory = null;
         try {
-            logger.debug("Loading configuration.");
-            config = new PropertiesConfiguration(CONFIGURATION_FILENAME);
-        } catch (ConfigurationException e) {
+            workingDirectory = Paths.get(Main.class.getProtectionDomain().getCodeSource().getLocation().toURI());
+            if (workingDirectory.toFile().isFile()) {
+                workingDirectory = workingDirectory.getParent();
+            }
+            logger.debug("Working Directory: " + workingDirectory);
+        } catch (URISyntaxException e) {
+            logger.error("Working directory could not be resolved!");
             logger.error(e.getMessage(), e);
-            // TODO Exception handling
+            logger.error("Exiting...");
+            System.exit(1);
         }
-
 
         options = new Options();
 
@@ -81,8 +84,34 @@ public class Main {
             // TODO Exception handling
         }
 
+        Path defaultConfigPath = workingDirectory.resolve(CONFIGURATION_FILENAME);
+        logger.debug("Searching for config in default filepath: " + defaultConfigPath.toAbsolutePath().toString());
+
+        try {
+            logger.debug("Loading configuration.");
+
+            config = new PropertiesConfiguration(defaultConfigPath.toFile());
+
+        } catch (ConfigurationException e) {
+            logger.error(e.getMessage(), e);
+            // TODO Exception handling
+        }
+
+        if (!(defaultConfigPath.toFile().exists() && defaultConfigPath.toFile().isFile())) {
+            try {
+                logger.debug("Creating empty config file in working directory...");
+                config.save();
+                logger.debug("Saved empty configuration: " + defaultConfigPath.toAbsolutePath().toString());
+            } catch (ConfigurationException e) {
+                logger.error(e.getMessage(), e);
+                // TODO Exception handling
+            }
+        }
+
+
         LatexToHtmlConverter latexToHtmlConverter = new PandocLatexToHtmlConverter();
 
+        // TODO input file --> convert call
         latexToHtmlConverter.convert(null);
 
         logger.debug("main() exit.");
